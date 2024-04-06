6b355ed1f6b261d4a243dff223594ff3775680b5
Aman1513 <amanmishra1513@gmail.com>
2024-04-06 22:57:36 +0530

    Category Crud

---
+++ CommericalWebMVC/Controllers/CategoryController.cs
@@ -0,0 +1,95 @@
+﻿using CommercialWebMVC.Data;
+using CommericalWebMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommercialWebMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryController( ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList = dbContext.Categories.ToList();
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order can't same as Name");
+            }
+            if (ModelState.IsValid)
+            {
+                dbContext.Categories.Add(obj);
+                dbContext.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Category? category = dbContext.Categories.Find(id);
+
+            if (category == null)
+               return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.Categories.Update(obj);
+                dbContext.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            Category? category = dbContext.Categories.Find(id);
+
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            Category? category = dbContext.Categories.Find(id);
+            if (category == null)
+                return NotFound();
+
+            dbContext.Categories.Remove(category);
+            dbContext.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
